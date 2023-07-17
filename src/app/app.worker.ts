fe1cd8b515a59ace575c7d7fad8000f332efd26d
mesut <ergul_mesut@yahoo.com>
2023-07-17 08:03:42 +0300

    eve

--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -1,20 +1,20 @@
 // import { EventSourcePolyfill } from 'event-source-polyfill';
 // import { BehaviorSubject } from 'rxjs';
-// import { RecordSSE } from './core/record.sse';
-// import { NewsPayload } from './core/news.model';
-// const tagsBehaviorSubject = new BehaviorSubject<RecordSSE[]>([]);
+// import { NewsPayload, OfferPayload } from './core/news.model';
+// const offersBehaviorSubject = new BehaviorSubject<OfferPayload[]>([]);
 // const newsBehaviorSubject = new BehaviorSubject<NewsPayload[]>([]);
 // addEventListener('message', ({ data }) => {
 //   let ev = JSON.parse(data[0]);
 //   let evy = data[1];
 //   Object.setPrototypeOf(ev, EventSourcePolyfill.prototype);
-//   ev.addEventListener('top-tags', (event) => {
-//     const topTags = JSON.parse(event.data);
-//     tagsBehaviorSubject.next(topTags.list);
+//   console.log('webworker: ', ev._listeners, ' number: ', evy);
+//   ev.addEventListener('top-offers', (event) => {
+//     const topOffers = JSON.parse(event.data);
+//     offersBehaviorSubject.next(topOffers.list);
 //   });
-//   ev.addEventListener('top-news-' + evy, (event) => {
+//   ev.addEventListener('top-news', (event) => {
 //     const topNews = JSON.parse(event.data);
 //     newsBehaviorSubject.next(topNews.list);
 //   });
-//   postMessage(JSON.stringify(tagsBehaviorSubject.value));
+//   postMessage(JSON.stringify(offersBehaviorSubject.value));
 // });
